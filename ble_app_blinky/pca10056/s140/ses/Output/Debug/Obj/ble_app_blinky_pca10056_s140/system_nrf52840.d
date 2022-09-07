564d59dc2a6e2ec19c5af88ed010e76f5c4f0d84
0Muhammad-Azeem <azeem.work352@gmail.com>
2022-09-07 14:19:16 +0500

    First commit for project

---
+++ ble_app_blinky/pca10056/s140/ses/Output/Debug/Obj/ble_app_blinky_pca10056_s140/system_nrf52840.d
@@ -0,0 +1,27 @@
+Output/Debug/Obj/ble_app_blinky_pca10056_s140/system_nrf52840.o: \
+ modules\nrfx\mdk\system_nrf52840.c modules\nrfx\mdk\system_nrf52.c \
+ C:/Users/Dell/AppData/Local/SEGGER/SEGGER\ Embedded\ Studio/v3/packages/libraries/libcxx/include/stdint.h \
+ C:/Users/Dell/AppData/Local/SEGGER/SEGGER\ Embedded\ Studio/v3/packages/libraries/libcxx/include/__config \
+ E:/NordicSemiconductors/include/stdint.h \
+ E:/NordicSemiconductors/include/__SEGGER_RTL.h \
+ E:/NordicSemiconductors/include/__SEGGER_RTL_ConfDefaults.h \
+ E:/NordicSemiconductors/include/__SEGGER_RTL_Conf.h \
+ E:/NordicSemiconductors/include/__SEGGER_RTL_Arm_Conf.h \
+ C:/Users/Dell/AppData/Local/SEGGER/SEGGER\ Embedded\ Studio/v3/packages/libraries/libcxx/include/stdbool.h \
+ E:/NordicSemiconductors/include/stdbool.h modules\nrfx\mdk\nrf.h \
+ modules\nrfx\mdk\nrf52840.h \
+ components/toolchain/cmsis/include/core_cm4.h \
+ components/toolchain/cmsis/include/cmsis_version.h \
+ components/toolchain/cmsis/include/cmsis_compiler.h \
+ components/toolchain/cmsis/include/cmsis_gcc.h \
+ components/toolchain/cmsis/include/mpu_armv7.h \
+ modules\nrfx\mdk\system_nrf52840.h modules\nrfx\mdk\nrf52840_bitfields.h \
+ modules\nrfx\mdk\nrf51_to_nrf52840.h \
+ modules\nrfx\mdk\nrf52_to_nrf52840.h \
+ modules\nrfx\mdk\compiler_abstraction.h \
+ modules\nrfx\mdk\nrf_peripherals.h \
+ modules\nrfx\mdk\nrf52840_peripherals.h modules\nrfx\mdk\nrf_erratas.h \
+ modules\nrfx\mdk\nrf51_erratas.h modules\nrfx\mdk\nrf52_erratas.h \
+ modules\nrfx\mdk\nrf53_erratas.h modules\nrfx\mdk\nrf91_erratas.h \
+ modules\nrfx\mdk\system_nrf52.h \
+ modules\nrfx\mdk\system_nrf52_approtect.h
---
+++ ble_app_blinky/pca10056/s140/ses/Output/Debug/Obj/ble_app_blinky_pca10056_s140/utf.d
@@ -0,0 +1,11 @@
+Output/Debug/Obj/ble_app_blinky_pca10056_s140/utf.o: \
+ external\utf_converter\utf.c external\utf_converter\utf.h \
+ C:/Users/Dell/AppData/Local/SEGGER/SEGGER\ Embedded\ Studio/v3/packages/libraries/libcxx/include/stddef.h \
+ C:/Users/Dell/AppData/Local/SEGGER/SEGGER\ Embedded\ Studio/v3/packages/libraries/libcxx/include/__config \
+ E:/NordicSemiconductors/include/stddef.h \
+ E:/NordicSemiconductors/include/__SEGGER_RTL.h \
+ E:/NordicSemiconductors/include/__SEGGER_RTL_ConfDefaults.h \
+ E:/NordicSemiconductors/include/__SEGGER_RTL_Conf.h \
+ E:/NordicSemiconductors/include/__SEGGER_RTL_Arm_Conf.h \
+ C:/Users/Dell/AppData/Local/SEGGER/SEGGER\ Embedded\ Studio/v3/packages/libraries/libcxx/include/stdint.h \
+ E:/NordicSemiconductors/include/stdint.h
